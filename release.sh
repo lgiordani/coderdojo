d1a5614352620ebb7120fa752ccf3989bc8ee1f9
Leonardo Giordani <giordani.leonardo@gmail.com>
2016-01-27 17:04:58 +0100

    Fixed release script

--- release.sh
+++ release.sh
@@ -4,4 +4,6 @@ version=$(date +%Y%m%d%H%M%S)
 
 git flow release start ${version}
 echo ${version} > release.txt
+git add release.txt
+git ci -m "Version bumped"
 git flow release finish -m "Version ${version}"
