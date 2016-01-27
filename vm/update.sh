fc9f3371b10f2d6b31ac438ae0baba2cede6170b
Leonardo Giordani <giordani.leonardo@gmail.com>
2016-01-27 17:40:43 +0100

    Added a log

--- vm/update.sh
+++ vm/update.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-if [[ ! $(ping -c 1 -w 2 www.google.com) ]]; then zenity --error --text "Check your internet connection"; exit 1; fi
-
 exec_dir=$(dirname ${BASH_SOURCE})
 registry="${exec_dir}/registry.list"
+log="${exec_dir}/update.log"
+
+echo "(date +%Y%m%d%H%M%S):Execution" >> ${log}
+
+if [[ ! $(ping -c 1 -w 2 www.google.com) ]]; then
+    zenity --error --text "Check your internet connection"
+    exit 1
+    echo "(date +%Y%m%d%H%M%S):Aborted due to missing Internet connection" >> ${log}
+fi
 
 if [[ ! -f ${registry} ]]; then touch ${registry}; fi
 
 for migration in $(find ${exec_dir}/migrations -iname "*.sh" | sort)
 do
-	migration_name=$(basename ${migration} | sed s,"\.sh",,)
-	if [[ ! -z $(grep -E "^${migration_name}$" ${registry}) ]]; then continue; fi
-	
-	echo "Running ${migration}"
-	echo "----------------------------------------------"
-	echo
-	bash ${migration}
-	echo
-	echo
-	echo "${migration_name}" >> ${registry}
+    migration_name=$(basename ${migration} | sed s,"\.sh",,)
+    if [[ ! -z $(grep -E "^${migration_name}$" ${registry}) ]]; then
+        echo "(date +%Y%m%d%H%M%S):Skipping already executed ${migration}" >> ${log}
+        continue
+    fi
+    
+    echo "Running ${migration}"
+    echo "----------------------------------------------"
+    echo
+    echo "(date +%Y%m%d%H%M%S):Running migration ${migration}" >> ${log}
+    bash ${migration}
+    echo
+    echo
+    echo "${migration_name}" >> ${registry}
 
 done
 
